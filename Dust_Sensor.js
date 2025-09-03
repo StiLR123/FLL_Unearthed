db9181709923dcbf5ef61eb5070899d09b32cf6d
StiLR123 <166840087+StiLR123@users.noreply.github.com>
2025-09-03 20:04:11 +0200

    Dust_Sensor.js

--- Dust_Sensor.js
+++ Dust_Sensor.js
@@ -1,9 +1,29 @@
-// stop logging
+// IT TOOK ME THE WHOLE EVENING TRYING TO LEARN JAVASCRIPT AND DO THIS (because the blocks were dumb and too confusing) SO PLEASE APPRECIATE THIS EVEN THOUGH IT IS JUST THE DUST SENSOR
+
+// At my house, SSID is "EP-WIFI" and PASS is "1234@#!5678"
+// Key is "SR13MH6FXNLXZY76"
+// Dust sensor vLED on P13, VO on P1
+// baud rate is 115200 (no idea what that is tbh)
+
+const WIFI_SSID = "SSID"
+const WIFI_PASS = "PASSWORD"
+const THINGSPEAK_WRITE_KEY = "SR13MH6FXNLXZY76"
+
+let logging = false
+let uploadIntervalMs = 60000
+datalogger.setColumnTitles("timestamp", "dust")
+
 datalogger.onLogFull(function () {
     logging = false
     basic.showIcon(IconNames.Skull)
 })
-// toggle logging with button A
+
+
+ESP8266ThingSpeak.connectWifi(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200, WIFI_SSID, WIFI_PASS)
+basic.showString("WIFI")
+ESP8266ThingSpeak.wait(3000)
+
+// start log
 input.onButtonPressed(Button.A, function () {
     logging = !(logging)
     if (logging) {
@@ -12,68 +32,37 @@ input.onButtonPressed(Button.A, function () {
         basic.clearScreen()
     }
 })
-// delete log with button A+B while logo pressed
+
+// delete log
 input.onButtonPressed(Button.AB, function () {
     if (input.logoIsPressed()) {
         basic.showIcon(IconNames.No)
         datalogger.deleteLog()
     }
     logging = false
-    datalogger.setColumnTitles(
-    "timestamp",
-    "dust"
-    )
+    datalogger.setColumnTitles("timestamp", "dust")
 })
-let dust = 0
-let dustVal = 0
-let logging = false
-let WIFI_SSID = "EP-WIFI"
-let WIFI_PASS = "1234@#!5678"
-let THINGSPEAK_WRITE_KEY = "SR13MH6FXNLXZY76"
-let uploadIntervalMs = 6000
 
-datalogger.setColumnTitles(
-"timestamp",
-"dust"
-)
-ESP8266ThingSpeak.connectWifi(
-SerialPin.P8,
-SerialPin.P12,
-BaudRate.BaudRate115200,
-WIFI_SSID,
-WIFI_PASS
-)
-basic.showString("WIFI")
-ESP8266ThingSpeak.wait(3000)
-basic.pause(1000)
-basic.showIcon(IconNames.Happy)
+loops.everyInterval(200, function () {
+    if (logging) {
+        let dust = Environment.ReadDust(DigitalPin.P13, AnalogPin.P1)
+        datalogger.log(
+            datalogger.createCV("timestamp", input.runningTime()),
+            datalogger.createCV("dust", dust)
+        )
+    }
+})
+
 loops.everyInterval(uploadIntervalMs, function () {
     if (logging) {
-        dustVal = Environment.ReadDust(DigitalPin.P13, AnalogPin.P1)
-        // send to ThingSpeak field1
-        ESP8266ThingSpeak.connectThingSpeak(
-        "api.thingspeak.com",
-        THINGSPEAK_WRITE_KEY,
-        dustVal,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0
-        )
+        let dustVal = Environment.ReadDust(DigitalPin.P13, AnalogPin.P1)
+        // send to field1
+        ESP8266ThingSpeak.connectThingSpeak("api.thingspeak.com", THINGSPEAK_WRITE_KEY, dustVal, 0, 0, 0, 0, 0, 0, 0)
         ESP8266ThingSpeak.wait(5000)
     }
 })
-loops.everyInterval(200, function () {
-    if (logging) {
-        dust = Environment.ReadDust(DigitalPin.P13, AnalogPin.P1)
-        datalogger.log(
-        datalogger.createCV("timestamp", input.runningTime()),
-        datalogger.createCV("dust", dust)
-        )
-    }
-})
+
+basic.pause(1000)
+basic.showIcon(IconNames.Happy)
 
 // show dust value on LED later
